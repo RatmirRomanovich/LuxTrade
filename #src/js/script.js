0a20de7252c589c7d92abb6dd812c0bb700b52d3
RatmirRomanovich <ratmir1806@yandex.ru>
2020-06-26 21:07:41 +0300

    header

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,21 @@
-//реализация выпадающего меню
+//!реализация выпадающего меню
 let user_icon = document.querySelector('.user-header__icon');
 user_icon.addEventListener('click', function (e) {
 	let user_menu = document.querySelector('.user-header__menu');
 	user_menu.classList.toggle('_active');
 });
+
+//!БУРГЕР меню
+let iconMenu = document.querySelector(".icon-menu");
+if (iconMenu != null) {
+	let delay = 500;
+	let body = document.querySelector("body");
+	let menuBody = document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", (e) => {
+		if (!body.classList.contains('_wait')) {
+			body_lock(delay);
+			iconMenu.classList.toggle("_active");
+			menuBody.classList.toggle("_active");
+		}
+	});
+};
