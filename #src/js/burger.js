6a7f30ad5d8699f846d8302abf4b82c8a2c84ddb
RatmirRomanovich <ratmir1806@yandex.ru>
2020-07-01 16:01:00 +0300

    the head is almost finished, the dynamic adaptive is not enough

---
+++ #src/js/burger.js
@@ -0,0 +1,12 @@
+//*(start)_ БУРГЕР меню.
+let iconMenu = document.querySelector(".icon-menu");
+let body = document.querySelector("body");
+let menuBody = document.querySelector(".menu__body");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function () {
+		iconMenu.classList.toggle("_active");
+		body.classList.toggle("lock");
+		menuBody.classList.toggle("_active");
+	});
+}
+//*(end)
--- LuxTrade/js/script.js
+++ LuxTrade/js/script.js
@@ -1,21 +1,37 @@
-//!реализация выпадающего меню
+//*(start)_ БУРГЕР меню.
+let iconMenu = document.querySelector(".icon-menu");
+let body = document.querySelector("body");
+let menuBody = document.querySelector(".menu__body");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function () {
+		iconMenu.classList.toggle("_active");
+		body.classList.toggle("lock");
+		menuBody.classList.toggle("_active");
+	});
+}
+//*(end);
+//*(start)_ реализация ВЫПАДАЮЩЕГО МЕНЮ.
 let user_icon = document.querySelector('.user-header__icon');
 user_icon.addEventListener('click', function (e) {
 	let user_menu = document.querySelector('.user-header__menu');
 	user_menu.classList.toggle('_active');
 });
+//*(end)
 
-//!БУРГЕР меню
-let iconMenu = document.querySelector(".icon-menu");
-if (iconMenu != null) {
-	let delay = 500;
-	let body = document.querySelector("body");
-	let menuBody = document.querySelector(".menu__body");
-	iconMenu.addEventListener("click", (e) => {
-		if (!body.classList.contains('_wait')) {
-			body_lock(delay);
-			iconMenu.classList.toggle("_active");
-			menuBody.classList.toggle("_active");
-		}
-	});
-};
+//*(start)_ скрытие ВЫПАДАЮЩЕГО МЕНЮ при клике на любую область вне его.
+document.addEventListener('click', function (e) {
+	if (!e.target.closest('.user-header')) {
+		let user_menu = document.querySelector('.user-header__menu');
+		user_menu.classList.remove('_active');
+	}
+});
+//*(end)
+
+//!(start)_ Динамический адаптив(автоматическая перестановка классов и расположения тегов в html при изменении ширины экрана, по сути перемещаем элеммент на страницы из одного места в другое)
+/*<a data-move="№_1 menu__body,№_2 1,№_3 767 " href="" class="actions-header__region">
+		<span>Выбор Региона</span>
+	</a> 
+	№_1- куда отправляем элемент
+	№_2- каким по счету он должен там быть(начиная с 0)
+	№_3- при каком разрешении*/
+//!(end)
