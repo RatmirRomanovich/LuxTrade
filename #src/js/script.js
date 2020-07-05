c6f0298d2718a33fd07e164b8e2c06e398443d4f
RatmirRomanovich <ratmir1806@yandex.ru>
2020-07-05 21:06:40 +0300

    section2 almost finished, not just a slider.

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,5 @@
-@@include('burger.js', {});
+@@include('slick.min.js');
+@@include('burger.js');
 
 //*(start)_ реализация ВЫПАДАЮЩЕГО МЕНЮ.
 let user_icon = document.querySelector('.user-header__icon');
@@ -25,3 +26,112 @@ document.addEventListener('click', function (e) {
 	№_2- каким по счету он должен там быть(начиная с 0)
 	№_3- при каком разрешении*/
 //!(end)
+
+//!Слайдер Start
+//SLIDERS
+if($('.mainslider').length>0){
+	$('.mainslider').slick({
+		//autoplay: true,
+		//infinite: false,
+		dots: true,
+		arrows: false,
+		accessibility:false,
+		slidesToShow:1,
+		autoplaySpeed: 3000,
+		//asNavFor:'',
+		//appendDots:
+		//appendArrows:$('.mainslider-arrows .container'),
+		nextArrow:'<button type="button" class="slick-next"></button>',
+		prevArrow:'<button type="button" class="slick-prev"></button>',
+		responsive: [{
+			breakpoint: 768,
+			settings: {}
+		}]
+	});
+}
+//BildSlider
+var sliders = document.querySelectorAll('._swiper');
+
+if (sliders) {
+	for (var _index24 = 0; _index24 < sliders.length; _index24++) {
+		var slider = sliders[_index24];
+
+		if (!slider.classList.contains('swiper-bild')) {
+			var slider_items = slider.children;
+
+			if (slider_items) {
+				for (var _index25 = 0; _index25 < slider_items.length; _index25++) {
+					var _el12 = slider_items[_index25];
+
+					_el12.classList.add('swiper-slide');
+				}
+			}
+
+			var slider_content = slider.innerHTML;
+			var slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+		}
+
+		if (slider.classList.contains('_gallery')) {//slider.data('lightGallery').destroy(true);
+		}
+	}
+
+	sliders_bild_callback();
+}
+
+function sliders_bild_callback(params) { }
+
+var lots_slider = new Swiper('.slider-lots__body', {
+	/*
+	effect: 'fade',
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	*/
+	observer: true,
+	observeParents: true,
+	slidesPerView: 3,//сколько слайдов нам показывать
+	spaceBetween: 0,
+	///autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	loop: true,
+	//preloadImages: false,
+	//lazy: true,
+	// Dotts
+	//pagination: {
+	//	el: '.slider-quality__pagging',
+	//	clickable: true,
+	//},
+	// Arrows
+	navigation: {
+		nextEl: '.control-slider-lots__arrow_next',
+		prevEl: '.control-slider-lots__arrow_prev',
+	},
+	/*
+	breakpoints: {
+		320: {
+			autoHeight: true
+		},
+		768: {
+			autoHeight: false
+		}
+	},
+	*/
+	on: {
+		lazyImageReady: function lazyImageReady() {
+			ibg();
+		}
+	} // And if we need scrollbar
+	//scrollbar: {
+	//	el: '.swiper-scrollbar',
+	//},
+
+});
+//!Слайдер End
